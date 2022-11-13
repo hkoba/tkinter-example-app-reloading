43b0d577cdc320f2cfe7fe90f9a0513f53d396e0
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-11-13 12:42:04 +0900

    Declarative widget construction `Widget` and reloading-aware `Redraw`

--- app1.py
+++ app1.py
@@ -9,6 +9,7 @@ import sys
 print("Hello!")
 
 def Reload():
+    print("Hello again!")
     if __name__ == "__main__":
         raise Exception("Can't use reload feature in toplevel module!")
     bn = os.path.basename(__file__)
@@ -22,21 +23,35 @@ def Reload():
 def Packed(widget, **kwargs):
     widget.pack(**kwargs)
     widget
+
+def Widget(parent, elName, type, **kwargs):
+    print("New widget")
+    pathName = parent._w + '.' + elName
+    print(pathName)
+    if parent.tk.getint(parent.tk.call('winfo', 'exists', pathName)):
+        w = parent.children[elName]
+        w.configure(**kwargs)
+        print("configure", elName, kwargs)
+        return w
+    else:
+        return type(parent, name=elName, **kwargs)
+
+def Redraw(self):
+    print("Redraw is called!")
+    if __name__ != "__main__":
+        self.reload_btn = Packed(Widget(self, 'reload', tk.Button, text="reload", command=Reload))
+
+    self.text = Packed(Widget(self, 'editor', tk.Text, width=120))
+    self.pack()
+
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.Redraw()
+        Redraw(self)
 
-
-    def Redraw(self):
-        if __name__ != "__main__":
-            self.reload_btn = Packed(tk.Button(self, text="reload", command=Reload))
-
-        self.text = Packed(tk.Text(self))
-        self.pack()
-
-if __name__ == "__main__":
-    if 'root' not in locals():
-        root = tk.Tk()
-        myapp = App(root)
-        myapp.mainloop()
+if 'root' not in locals():
+    root = tk.Tk()
+    myapp = App(root)
+    myapp.mainloop()
+else:
+    Redraw(myapp)
--- main1.py
+++ main1.py
@@ -3,7 +3,3 @@
 
 import tkinter as tk
 import app1
-
-root = tk.Tk()
-myapp = app1.App(root)
-myapp.mainloop()
